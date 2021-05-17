6ab3bf49365f0a81e3d55ebc6a0a4ab56f8fe1ef
BSKYB\RobertG <gareth.roberts@sky.uk>
2021-05-17 14:56:07 +0100

    initial commit with Spring Boot

---
+++ src/main/java/com/example/NasaInterview/NasaInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.NasaInterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NasaInterview/NasaInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NasaInterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
